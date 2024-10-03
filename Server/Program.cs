155eca2b48ef199a7fd2727a47559164e3e9c0aa
vitheshshetty00 <vitheshshetty23@gmail.com>
2024-10-03 10:36:10 +0530

    Add Base64Helpers

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using Microsoft.Data.SqlClient;
+using Server.DbAccess;
 using Shared.Utils;
+using System.Data;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello,world");
--- Shared/Utils/Base64Helper.cs
+++ Shared/Utils/Base64Helper.cs
@@ -1,8 +1,43 @@
-﻿
+﻿using System.Data;
+using System.Text;
+using System.Xml;
 
 namespace Shared.Utils
 {
     public static class Base64Helper
     {
+        public static string EncodeDatatable(this DataTable dataTable)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var xmlWriter = XmlWriter.Create(memoryStream, new XmlWriterSettings { Encoding = Encoding.UTF8 }))
+                {
+                    dataTable.WriteXml(xmlWriter, XmlWriteMode.WriteSchema);
+                }
+                return Base64Helper.Encode(memoryStream.ToArray());
+            }
+        }
+
+        public static DataTable DecodeDatatable(string base64)
+        {
+            var dataTable = new DataTable();
+            using (var memoryStream = new MemoryStream(Base64Helper.Decode(base64)))
+            {
+                using (var xmlReader = XmlReader.Create(memoryStream))
+                {
+                    dataTable.ReadXml(xmlReader);
+                }
+            }
+            return dataTable;
+        }
+        public static string Encode(byte[] data)
+        {
+            return System.Convert.ToBase64String(data);
+        }
+
+        public static byte[] Decode(string base64)
+        {
+            return System.Convert.FromBase64String(base64);
+        }
     }
 }
