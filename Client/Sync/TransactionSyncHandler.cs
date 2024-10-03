026ddf8db2236cb613ae743c33669b649aaf0067
vitheshshetty00 <vitheshshetty23@gmail.com>
2024-10-03 14:52:49 +0530

    Add Transaction sync to server

--- Client/Sync/TransactionSyncHandler.cs
+++ Client/Sync/TransactionSyncHandler.cs
@@ -1,12 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Client.DbAccess;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using static Shared.Utils.Base64Helper;
+
 
 namespace Client.Sync
 {
-    internal class TransactionSyncHandler
+    public class TransactionSyncHandler
     {
+        private NetworkStream networkStream;
+        private Timer? _syncTimer;
+
+        public TransactionSyncHandler(NetworkStream networkStream)
+        {
+            this.networkStream = networkStream;
+        }
+
+        public void Start()
+        {
+            _syncTimer = new Timer(SyncTransaction, null, TimeSpan.Zero,TimeSpan.FromMinutes(2));
+        }
+
+        private void SyncTransaction(object? state)
+        {
+            Console.WriteLine("Transaction sync started.");
+
+            DataTable transactionTable = GetRecentTransactions();
+
+            if (transactionTable.Rows.Count > 0)
+            {
+                string transactionBase64 =EncodeDatatable(transactionTable);
+                SendDataToServer(transactionBase64);
+            }
+            else
+            {
+                Console.WriteLine("No New Transactions to Sync.");
+            }
+            Console.WriteLine("Transaction sync completed.");
+
+        }
+
+        private void SendDataToServer(string base64Data)
+        {
+            try
+            {
+                byte[] dataToSend = Encoding.UTF8.GetBytes(base64Data);
+
+                networkStream.Write(dataToSend, 0, dataToSend.Length);
+                networkStream.Flush();
+                Console.WriteLine("Data sent to server.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending data to server: {ex.Message}");
+            }
+        }
+
+        private DataTable GetRecentTransactions()
+        {
+            DataTable transactionTable = new DataTable();
+
+            SqlConnection connection = DbConnectionManager.GetConnection();
+            try
+            {
+                string query = @"SELECT * FROM TransactionTableMaster
+                        WHERE Timestamp >= DATEADD(MINUTE, -2, GETDATE())";
+                using (SqlCommand command = new SqlCommand(query,connection))
+                {
+                    using(SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(transactionTable);
+                    }
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(String.Format("An SQL Exception During Getting the Recent Transaction: {0}",ex.Message));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("An Exception During Getting the Recent Transaction: {0}", ex.Message));
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return transactionTable;
+        }
     }
 }
+
--- Shared/Utils/Base64Helper.cs
+++ Shared/Utils/Base64Helper.cs
@@ -1,43 +1,37 @@
 ﻿using System.Data;
-using System.Text;
 using System.Xml;
 
 namespace Shared.Utils
 {
     public static class Base64Helper
     {
-        public static string EncodeDatatable(this DataTable dataTable)
+        public static string EncodeDatatable(DataTable dataTable)
         {
             using (var memoryStream = new MemoryStream())
             {
-                using (var xmlWriter = XmlWriter.Create(memoryStream, new XmlWriterSettings { Encoding = Encoding.UTF8 }))
-                {
-                    dataTable.WriteXml(xmlWriter, XmlWriteMode.WriteSchema);
-                }
-                return Base64Helper.Encode(memoryStream.ToArray());
+                    dataTable.WriteXml(memoryStream);
+               
+                return Encode(memoryStream.ToArray());
             }
         }
 
         public static DataTable DecodeDatatable(string base64)
         {
             var dataTable = new DataTable();
-            using (var memoryStream = new MemoryStream(Base64Helper.Decode(base64)))
-            {
-                using (var xmlReader = XmlReader.Create(memoryStream))
-                {
-                    dataTable.ReadXml(xmlReader);
-                }
+            using (var memoryStream = new MemoryStream(Decode(base64)))
+            {   
+                    dataTable.ReadXml(memoryStream);
             }
             return dataTable;
         }
         public static string Encode(byte[] data)
         {
-            return System.Convert.ToBase64String(data);
+            return Convert.ToBase64String(data);
         }
 
         public static byte[] Decode(string base64)
         {
-            return System.Convert.FromBase64String(base64);
+            return Convert.FromBase64String(base64);
         }
     }
 }
