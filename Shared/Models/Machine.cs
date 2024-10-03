6ae801a25ec1fe9fccad02a05a4c5ac920b4fe78
vitheshshetty00 <vitheshshetty23@gmail.com>
2024-10-03 09:45:47 +0530

    Add models

---
+++ Shared/Models/Machine.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Models
+{
+    public class Machine
+    {
+        public string? MachineId { get; set; }
+        public string? MachineName { get; set; }
+        public string? IPAddress { get; set; }
+        public int? PortNumber { get; set; }
+        public byte[]? Image { get; set; }
+        public DateTime? Timestamp { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}
---
+++ Shared/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+
+    public class Transaction
+    {
+        public int? TransactionId { get; set; }
+        public int? M_Id { get; set; }
+        public EventType? EventType { get; set; }
+        public DateTime? Timestamp { get; set; }
+        public TransactionStatus? Status { get; set; }
+
+    }
+
+    public enum TransactionStatus
+    {
+        Success,
+        Failure
+    }
+
+    public enum EventType
+    {
+        Ping,
+        TelNet
+    }
+}
